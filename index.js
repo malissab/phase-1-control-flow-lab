83fc94d2ed95691aa20ecd73aab677b4225b0ca7
Malissa Bishop <malissambishop7@gmail.com>
2021-10-12 20:34:36 -0700

    commit tree 51a126ca5070bfa93bd673c25fbd65fa8fef0aac

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(aRide){
+  if (aRide <= 400) {
+    return 'This one is on me!'
+  } else if (aRide >= 2000 && aRide <= 2500 ) {
+    return 'I will gladly take your thirty bucks.'
+  } else {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return (city === "NYC" ? "Ok, sounds good." : "No go.")
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tips){
   // Write your code here!
+   switch (tips) {
+     case 'generous':
+       return 'Thank you so much.';
+       break;
+      case 'not as generous':
+        return 'Thank you.'
+        break;
+      default:
+        return 'Bye.'
+   } 
 }
